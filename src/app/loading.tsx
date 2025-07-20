34bb8bbcbd0b00264e947d93f8ffc8fe03fef2ca
Hassaan Ali <mrhassaan929@gmail.com>
2025-07-20 18:37:05 +0000

    Loading state added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from 'lucide-react';
 export default function Loading() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <Loader2 className="h-16 w-16 animate-spin text-primary" />
+      <Loader2 className="h-16 w-16 animate-spin text-[#5D007A] dark:text-white" />
     </div>
   );
 }
